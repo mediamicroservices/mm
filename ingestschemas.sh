dedc9e01aa774f4ccf470ae61f9ed603ead46b7b
Andrew Weaver <weevz@uw.edu>
2017-02-10 11:27:11 -0500

    add variables and functions

--- ingestschemas.sh
+++ ingestschemas.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+SCRIPTDIR=$(dirname $(which "${0}"))
+. "${SCRIPTDIR}/mmfunctions"
 
 if [ -d "${1}" ] ; then
     cd "${1}"
@@ -6,6 +8,15 @@ else
     Echo "Please use a valid directory for input - Exiting" && exit 0
 fi
 
-for i in *.schema ; do
+if [ -z "${PREMIS_NAME}" ] || [ "${PREMIS_DB}" != "Y" ] ; then
+    echo "Please configure database options in mmconfig. Exiting"
+fi
 
+for i in *.schema ; do
+xmlschema="${i}"
+MEDIA_ID=$(basename "${xmlschema}" | cut -d'.' -f1)
+eventType="ingest"
+_report_to_db
+_report_schema_db
+_eventoutcome_update
 done
